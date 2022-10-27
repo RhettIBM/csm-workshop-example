cf30aa038b3dab5347955fdd34b8dc542105f840
Rhewid <102160136+RhettIBM@users.noreply.github.com>
2022-10-27 22:10:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import time
 hostName = "localhost"
 serverPort = 8080 #You can choose any available port; by default, it is 8000
 class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self): //the do_GET method is inherited from BaseHTTPRequestHandler
+    def do_GET(self): #the do_GET method is inherited from BaseHTTPRequestHandler
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
