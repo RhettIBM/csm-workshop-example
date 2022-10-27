5ddf065b30228ea7343b4c4db1afbffe69a0c99c
Rhewid <102160136+RhettIBM@users.noreply.github.com>
2022-10-27 22:21:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
-hName = "localhost"
 Port = 8080
 
 
 class wser (BaseHTTPRequestHandler):
     def do_GET (self):
-        self.send_response (200)
-        self.send_header ("Content-type", "text/html")
-        self.end_headers ()
-        self.wfile.write (bytes ("<html><head><title>CSM rocks</title></head>", "utf-8"))
-        self.wfile.write (bytes ("<body>", "utf-8"))
-        self.wfile.write (bytes ("<p>Hello CSM 2022</p>", "utf-8"))
-        self.wfile.write (bytes ("</body></html>", "utf-8"))
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes ("<html><head><title>CSM rocks</title></head>", "utf-8"))
+        self.wfile.write(bytes ("<body>", "utf-8"))
+        self.wfile.write(bytes ("<p>Hello CSM 2022</p>", "utf-8"))
+        self.wfile.write(bytes ("</body></html>", "utf-8"))
 
 
 if __name__ == "__main__":
-    wser = HTTPServer ((hName, Port), wser)
+    wser = HTTPServer(("0.0.0.0", Port), wser)
     wser.serve_forever()
